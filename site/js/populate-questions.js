7604eee8be5c32a0823daadf98d3686e6e0c851b
Kurt Leadley <kleadley@my365.bellevue.edu>
2019-09-13 04:26:21 -0400

    prototype

--- site/js/populate-questions.js
+++ site/js/populate-questions.js
@@ -1,35 +1,15 @@
 $(function() {
-    // JSON object to hold all our questions and answers - kl
-    // add some questions here
-    var questions = {
-        "one": {
-            "question": "What does js stand for?",
-            "answerOne":"jQuery Script",
-            "answerTwo":"Java Serialized",
-            "answerThree":"JSONScript",
-            "answerFour":"JavaScript",
-            "correctAnswer":"answerFour"
-        },
-        "two": {
-            "question":"How do you print a message to the console?",
-            "answerOne":"Logger.log()",
-            "answerTwo":"console.log()",
-            "answerThree":"console.log[]",
-            "answerFour":"Console.log()",
-            "correctAnswer":"answerTwo"
-        }
-    }
     // qId is used for creating unique ids for each question in our questions object, we increment it +1 every loop -kl
-    qId=1;
+    var qId=1;
     // get each question -kl
     for (var key in questions) {
         // this will return each parent key of our JSON object (one, two, three, four etc) -kl
         var question = questions[key];
         // using template literals for the extended html var - kl, source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals -kl
         // ${} is how to insert a js variable inside a template literal -kl
-        var html = 
-        `<section class='question-container'>
-            <div id='question-${qId}' class='question'><h2>${question.question}</h2></div>
+        var questionHtml = 
+        `<section id='q${qId}' class='question-container'>
+            <div id='question-q${qId}' class='question'><h3>#${qId}: ${question.question}</h3></div>
             <div id='answer-one-q${qId}' class='answer'><p>${question.answerOne}</p></div>
             <div id='answer-two-q${qId}' class='answer'><p>${question.answerTwo}</p></div>
             <div id='answer-three-q${qId}' class='answer'><p>${question.answerThree}</p></div>
@@ -37,8 +17,13 @@ $(function() {
             <div id='correct-incorrect-q${qId}' class='question-result'></div>
         </section>`
         // add our html string variable to the main element in our html document -kl
-        $('main').append(html);
+        $('.questions-container').append(questionHtml);
         // +1 for the next id -kl
         qId++;
+    }
+    for (var i=0; i < 10; i++) {
+        var rId = i + 1;
+        var resultsHtml = `<div id='q-tab-${rId}' class='q-tab'><p>Question ${rId}</p></div>`;
+        $('.q-tabs-container').append(resultsHtml);
     }
 });
